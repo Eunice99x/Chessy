39705f5801ad58eadc88aad048b7038fbf52a9cf
Younes Outerbah <younesouterbah1@gmail.com>
2024-03-22 06:45:00 +0100

    made board tiles move around the board

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import React, { ReactNode, useEffect, useRef } from "react";
 import Tile from "./Tile";
 
 interface Piece {
@@ -28,7 +28,7 @@ for (let p = 0; p < 8; p++) {
   const type = p === 0 ? "b" : "w";
   const x = p === 0 ? 7 : 0;
 
-  const pieceTypes = ["b", "n", "b", "q", "k", "b", "n", "r"];
+  const pieceTypes = ["r", "n", "b", "q", "k", "b", "n", "r"];
 
   for (let y = 0; y < pieceTypes.length; y++) {
     pieces.push({
@@ -40,6 +40,49 @@ for (let p = 0; p < 8; p++) {
 }
 
 export default function Board() {
+  const boardRef = useRef<HTMLDivElement>(null);
+
+  let activePiece: HTMLElement | null = null;
+  let offsetX = 0;
+  let offsetY = 0;
+
+  function grabPiece(e: React.MouseEvent) {
+    const element = e.target as HTMLElement;
+
+    if (element.classList.contains("piece")) {
+      offsetX = e.clientX - element.getBoundingClientRect().left;
+      offsetY = e.clientY - element.getBoundingClientRect().top;
+
+      activePiece = element;
+    }
+  }
+
+  function movePiece(e: React.MouseEvent) {
+    const boardTable = boardRef.current?.getBoundingClientRect();
+    if (activePiece && boardTable) {
+      const pieceWidth = activePiece.getBoundingClientRect().width;
+      const pieceHeight = activePiece.getBoundingClientRect().height;
+
+      let x = e.clientX - offsetX;
+      let y = e.clientY - offsetY;
+
+      // Ensure the piece stays within the boundaries of the board
+      x = Math.max(boardTable.left, Math.min(boardTable.right - pieceWidth, x));
+      y = Math.max(boardTable.top, Math.min(boardTable.bottom - pieceHeight, y));
+
+      activePiece.style.left = `${x}px`;
+      activePiece.style.top = `${y}px`;
+      activePiece.style.position = "absolute";
+      activePiece.style.width = "74.88px";
+    }
+  }
+
+  function dropPiece(e: React.MouseEvent) {
+    if (activePiece) {
+      activePiece = null;
+    }
+  }
+
   let board: ReactNode[] = [];
   for (let i = 7; i >= 0; i--) {
     for (let j = 0; j < 8; j++) {
@@ -52,8 +95,18 @@ export default function Board() {
         }
       });
 
-      board.push(<Tile image={image} number={number} />);
+      board.push(<Tile key={`${i}, ${j}`} image={image} number={number} />);
     }
   }
-  return <div className="size-[100vh] mx-auto grid grid-cols-8 grid-rows-8">{board}</div>;
+  return (
+    <div
+      onMouseDown={(e) => grabPiece(e)}
+      onMouseMove={(e) => movePiece(e)}
+      onMouseUp={(e) => dropPiece(e)}
+      ref={boardRef}
+      className="board size-[100vh] mx-auto grid grid-cols-8 grid-rows-8"
+    >
+      {board}
+    </div>
+  );
 }
--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -7,13 +7,13 @@ export default function Tile({ image, number }: Props) {
   if (number % 2 === 0) {
     return (
       <div className="bg-[#769656]">
-        <img src={image} alt="" />
+        <img draggable="false" className="piece" src={image} alt="" />
       </div>
     );
   } else {
     return (
       <div className="bg-[#eeeed2]">
-        <img src={image} alt="" />
+        <img draggable="false" className="piece" src={image} alt="" />
       </div>
     );
   }
