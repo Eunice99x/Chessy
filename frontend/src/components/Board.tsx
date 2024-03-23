3d34201fc5647bf26788a9e70b365cf5c8ecc61c
Younes Outerbah <younesouterbah1@gmail.com>
2024-03-23 05:37:45 +0100

    finally get the piece work fine

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useRef } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 import Tile from "./Tile";
 
 interface Piece {
@@ -7,39 +7,40 @@ interface Piece {
   y: number;
 }
 
-const pieces: Piece[] = [];
+const initialBoardState: Piece[] = [];
 
-for (let i = 0; i < 8; i++) {
-  pieces.push({
-    image: "https://images.chesscomfiles.com/chess-themes/pieces/light/150/bp.png",
-    x: 6,
-    y: i,
-  });
-}
-for (let i = 0; i < 8; i++) {
-  pieces.push({
-    image: "https://images.chesscomfiles.com/chess-themes/pieces/light/150/wp.png",
-    x: 1,
-    y: i,
-  });
-}
-
-for (let p = 0; p < 8; p++) {
+for (let p = 0; p < 2; p++) {
   const type = p === 0 ? "b" : "w";
-  const x = p === 0 ? 7 : 0;
+  const y = p === 0 ? 7 : 0;
 
   const pieceTypes = ["r", "n", "b", "q", "k", "b", "n", "r"];
 
-  for (let y = 0; y < pieceTypes.length; y++) {
-    pieces.push({
-      image: `https://images.chesscomfiles.com/chess-themes/pieces/light/150/${type}${pieceTypes[y]}.png`,
+  for (let x = 0; x < pieceTypes.length; x++) {
+    initialBoardState.push({
+      image: `https://images.chesscomfiles.com/chess-themes/pieces/light/150/${type}${pieceTypes[x]}.png`,
       x,
       y,
     });
   }
 }
 
+for (let i = 0; i < 8; i++) {
+  initialBoardState.push({
+    image: "https://images.chesscomfiles.com/chess-themes/pieces/light/150/bp.png",
+    x: i,
+    y: 6,
+  });
+}
+for (let i = 0; i < 8; i++) {
+  initialBoardState.push({
+    image: "https://images.chesscomfiles.com/chess-themes/pieces/light/150/wp.png",
+    x: i,
+    y: 1,
+  });
+}
+
 export default function Board() {
+  const [pieces, setPieces] = useState<Piece[]>(initialBoardState);
   const boardRef = useRef<HTMLDivElement>(null);
 
   let activePiece: HTMLElement | null = null;
@@ -58,35 +59,63 @@ export default function Board() {
   }
 
   function movePiece(e: React.MouseEvent) {
-    const boardTable = boardRef.current?.getBoundingClientRect();
-    if (activePiece && boardTable) {
-      const pieceWidth = activePiece.getBoundingClientRect().width;
-      const pieceHeight = activePiece.getBoundingClientRect().height;
+    const chessBoard = boardRef.current;
+    if (activePiece && chessBoard) {
+      const minX = chessBoard.offsetLeft - 25;
+      const minY = chessBoard.offsetTop - 25;
+      const maxX = chessBoard.offsetLeft + chessBoard.clientWidth - 75;
+      const maxY = chessBoard.offsetTop + chessBoard.clientHeight - 75;
 
-      let x = e.clientX - offsetX;
-      let y = e.clientY - offsetY;
-
-      // Ensure the piece stays within the boundaries of the board
-      x = Math.max(boardTable.left, Math.min(boardTable.right - pieceWidth, x));
-      y = Math.max(boardTable.top, Math.min(boardTable.bottom - pieceHeight, y));
-
-      activePiece.style.left = `${x}px`;
-      activePiece.style.top = `${y}px`;
+      const x = e.clientX - 50;
+      const y = e.clientY - 50;
       activePiece.style.position = "absolute";
       activePiece.style.width = "74.88px";
+
+      if (x < minX) {
+        activePiece.style.left = `${minX}px`;
+      } else if (x > maxX) {
+        activePiece.style.left = `${maxX}px`;
+      } else {
+        activePiece.style.left = `${x}px`;
+      }
+      if (y < minY) {
+        activePiece.style.top = `${minY}px`;
+      } else if (y > maxY) {
+        activePiece.style.top = `${maxY}px`;
+      } else {
+        activePiece.style.top = `${y}px`;
+      }
     }
   }
 
   function dropPiece(e: React.MouseEvent) {
-    if (activePiece) {
+    const chessBoard = boardRef.current;
+    if (activePiece && chessBoard) {
+      const x = Math.floor((e.clientX - chessBoard.offsetLeft) / 74.88);
+      const y = Math.abs(Math.ceil((e.clientY - chessBoard.offsetTop - 599) / 74.88));
+
+      console.log(x, y);
+
+      setPieces((value) => {
+        const pieces = value.map((p) => {
+          // the condition in the if is white night
+          if (p.x === 1 && p.y === 0) {
+            p.x = x;
+            p.y = y;
+          }
+          return p;
+        });
+        return pieces;
+      });
+
       activePiece = null;
     }
   }
 
   let board: ReactNode[] = [];
-  for (let i = 7; i >= 0; i--) {
-    for (let j = 0; j < 8; j++) {
-      const number = i + j + 2;
+  for (let j = 7; j >= 0; j--) {
+    for (let i = 0; i < 8; i++) {
+      const number = j + i + 2;
       let image = undefined;
 
       pieces.forEach((p) => {
@@ -95,7 +124,7 @@ export default function Board() {
         }
       });
 
-      board.push(<Tile key={`${i}, ${j}`} image={image} number={number} />);
+      board.push(<Tile key={`${j}, ${i}`} image={image} number={number} />);
     }
   }
   return (
--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -1,20 +1,14 @@
 interface Props {
-  image: string;
+  image?: string;
   number: number;
 }
 
 export default function Tile({ image, number }: Props) {
-  if (number % 2 === 0) {
-    return (
-      <div className="bg-[#769656]">
-        <img draggable="false" className="piece" src={image} alt="" />
-      </div>
-    );
-  } else {
-    return (
-      <div className="bg-[#eeeed2]">
-        <img draggable="false" className="piece" src={image} alt="" />
-      </div>
-    );
-  }
+  const isDark = number % 2 === 0; // Check if the tile is dark or light
+
+  return (
+    <div className={isDark ? "bg-[#769656]" : "bg-[#eeeed2]"}>
+      {image && <img draggable="false" className="piece" src={image} alt="" />}
+    </div>
+  );
 }
